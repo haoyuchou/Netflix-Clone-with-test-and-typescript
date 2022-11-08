e8c311719bed4627ddfc56a16ea56f62dbced960
haoyuchou <haoyuchou123@gmail.com>
2022-11-08 22:33:43 +0800

    style loading spinner at screen center

--- pages/video/index.tsx
+++ pages/video/index.tsx
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import dynamic from 'next/dynamic'
-const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
+import ReactPlayer from "react-player";
+import { TailSpin } from "react-loader-spinner";
 
 function index() {
+  const [hasWindow, setHasWindow] = useState(false);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setHasWindow(true);
+    }
+  }, []);
   const router = useRouter();
   const { youtubeKey } = router.query;
+  if (hasWindow) {
+    return (
+      <div className="h-screen">
+        <ReactPlayer
+          width="100%"
+          height="100%"
+          url={`https://www.youtube.com/watch?v=${youtubeKey}&origin=http://localhost:3000`}
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div className="h-screen pt-[25%]">
+        {/* Center the loading wrapper */}
 
-  return (
-    <div className="h-screen">
-      <ReactPlayer width="100%" height="100%" url={`https://www.youtube.com/watch?v=${youtubeKey}&origin=http://localhost:3000`}/>
-    </div>
-  );
+        <TailSpin
+          color="#E50914"
+          width="100%"
+          height="80"
+          ariaLabel="tail-spin-loading"
+          radius="1"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+        />
+      </div>
+    );
+  }
 }
 
 export default index;
